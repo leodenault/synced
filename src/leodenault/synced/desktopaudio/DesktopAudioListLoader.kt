41d4f11dc6bb66465cc9fa07b65486bdfabee266
Leo Denault <leo.denault@gmail.com>
2022-11-04 01:09:56 -0400

    Enable support for more audio file formats.
    Now that we're actually checking whether an audio file is playable, we
    can also leverage the Lavaplayer library to help us do an initial file
    filter based on audio extensions.

--- src/leodenault/synced/desktopaudio/DesktopAudioListLoader.kt
+++ src/leodenault/synced/desktopaudio/DesktopAudioListLoader.kt
@@ -1,6 +1,7 @@
 package leodenault.synced.desktopaudio
 
 import com.sedmelluq.discord.lavaplayer.container.MediaContainer
+import com.sedmelluq.discord.lavaplayer.container.MediaContainerHints
 import com.sedmelluq.discord.lavaplayer.container.MediaContainerProbe
 import com.sedmelluq.discord.lavaplayer.source.local.LocalSeekableInputStream
 import com.sedmelluq.discord.lavaplayer.track.AudioReference
@@ -12,50 +13,79 @@ import javax.inject.Inject
 
 @Reusable
 class DesktopAudioListLoader @Inject constructor(private val topLevelDirectory: Data<File>) {
-    fun loadAudio(): List<AudioFile> =
-        findAudioFilesRecursively(listOf(topLevelDirectory.value), emptyList())
+  fun loadAudio(): List<AudioFile> =
+    findAudioFilesRecursively(listOf(topLevelDirectory.value), emptyList())
 
-    private tailrec fun findAudioFilesRecursively(
-        dirs: List<File>,
-        audioFiles: List<AudioFile>
-    ): List<AudioFile> {
-        val children = dirs.asSequence().flatMap {
-            it.listFiles()?.toList() ?: emptyList()
-        }
-        val childAudioFiles = children.filter { child ->
-            child.isFile && VALID_FILE_EXTENSIONS.any { child.name.endsWith(it) }
-        }.map { AudioFile(it, isFileSupported(it)) }.toList()
-        val childDirs = children.filter { it.isDirectory }.toList()
-
-        if (childDirs.isEmpty()) {
-            return childAudioFiles + audioFiles
-        }
-
-        return findAudioFilesRecursively(childDirs, childAudioFiles + audioFiles)
+  private tailrec fun findAudioFilesRecursively(
+    dirs: List<File>,
+    audioFiles: List<AudioFile>
+  ): List<AudioFile> {
+    val children = dirs.asSequence().flatMap {
+      it.listFiles()?.toList() ?: emptyList()
     }
-
-    private fun isFileSupported(file: File): Boolean = kotlin.runCatching {
-        val probeResult: MediaContainerProbe? = MediaContainer.asList().firstOrNull {
-            // The probe returns null when the media container doesn't match.
-            it.probe(
-                AudioReference(file.canonicalPath, file.canonicalPath),
-                LocalSeekableInputStream(file)
-            )?.isSupportedFile ?: false
-        }
-        probeResult != null
-    }.getOrElse {
-        // Log this as info since sometimes the exception is WAI according to the library
-        // maintainer.
-        LOGGER.fine(
-            "Exception was thrown while attempting to load audio with message \"${it.message}\"." +
-                "\n${it.stackTraceToString()}"
+    val mediaContainers = MediaContainer.values()
+    val childAudioFiles = children.filter { child ->
+      child.isFile && mediaContainers.any { child.extension == it.fileExtension }
+    }.map { child ->
+      AudioFile(child, mediaContainers.any {
+        it.probe.fullySupports(
+          AudioReference(child.canonicalPath, child.canonicalPath),
+          LocalSeekableInputStream(child)
         )
-        false
+      })
+    }.toList()
+    val childDirs = children.filter { it.isDirectory }.toList()
+
+    if (childDirs.isEmpty()) {
+      return childAudioFiles + audioFiles
     }
 
-    companion object {
-        private val LOGGER =
-            Logger.getLogger("leodenault.synced.desktopaudio.DesktopAudioListLoader")
-        private val VALID_FILE_EXTENSIONS = listOf(".mp3", ".wav", ".ogg")
+    return findAudioFilesRecursively(childDirs, childAudioFiles + audioFiles)
+  }
+
+  /**
+   * Lavaplayer doesn't seem to have a way to programmatically validate file extensions, so we use
+   * an exhaustive `when` statement to help cover all extensions supported by the library.
+   *
+   * @return the file extension supported by this [MediaContainer] or `null` if unsupported.
+   */
+  private val MediaContainer.fileExtension: String?
+    get() = when (this) {
+      MediaContainer.WAV -> "wav"
+      MediaContainer.MKV -> "mka"
+      MediaContainer.MP4 -> "mp4"
+      MediaContainer.FLAC -> "flac"
+      MediaContainer.OGG -> "ogg"
+      MediaContainer.M3U -> ".m3u"
+      MediaContainer.PLS -> null
+      MediaContainer.PLAIN -> null
+      MediaContainer.MP3 -> "mp3"
+      MediaContainer.ADTS -> "adts"
+      MediaContainer.MPEGADTS -> "ts"
     }
+
+  /**
+   * Returns true if a file is supported by a [MediaContainerProbe] using a more thorough check than
+   * the original [MediaContainerDetectionResult.isSupportedFile].
+   */
+  private fun MediaContainerProbe.fullySupports(
+    audioReference: AudioReference,
+    inputStream: LocalSeekableInputStream
+  ): Boolean = kotlin.runCatching {
+    // The probe returns null when the media container doesn't match.
+    probe(audioReference, inputStream)?.isSupportedFile ?: false
+  }.getOrElse {
+    // Log this as info since sometimes the exception is WAI according to the library
+    // maintainer.
+    LOGGER.fine(
+      "Exception was thrown while attempting to load audio with message \"${it.message}\"." +
+          "\n${it.stackTraceToString()}"
+    )
+    false
+  }
+
+  companion object {
+    private val LOGGER =
+      Logger.getLogger("leodenault.synced.desktopaudio.DesktopAudioListLoader")
+  }
 }
