0ee2141f6a2ea8985da8daabda55109a0ac59869
Leo Denault <leo.denault@gmail.com>
2023-10-04 22:58:52 -0400

    Refactor player controls.
    *   Move buttons into a single view.
    *   Replace icons with custom SVGs.
    *   Introduce `resources` directory for hosting icons
        *   The directory _must_ be under `src/main` for Jetpack Compose
    resource loading to work.

--- src/leodenault/synced/player/PlayerControlsView.kt
+++ src/leodenault/synced/player/PlayerControlsView.kt
@@ -7,30 +7,73 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 
 @Composable
 fun PlayerControls(
-    modifier: Modifier = Modifier,
-    isAudioPlaying: Boolean,
-    onPlayButtonClick: () -> Unit = {},
-    onNextTrackClick: () -> Unit = {},
-    onPreviousTrackClick: () -> Unit = {}
+  modifier: Modifier = Modifier,
+  isAudioPlaying: Boolean,
+  onPlayButtonClick: () -> Unit = {},
+  onNextTrackClick: () -> Unit = {},
+  onPreviousTrackClick: () -> Unit = {}
 ) {
-    Row(
-        modifier = modifier,
-        horizontalArrangement = Arrangement.Center,
-        verticalAlignment = Alignment.CenterVertically
+  Row(
+    modifier = modifier,
+    horizontalArrangement = Arrangement.Center,
+    verticalAlignment = Alignment.CenterVertically
+  ) {
+    val buttonModifier = Modifier.size(80.dp).padding(5.dp)
+    val buttonColors = ButtonDefaults.buttonColors(backgroundColor = MaterialTheme.colors.secondary)
+    val buttonTint = MaterialTheme.colors.onSecondary
+
+    Button(
+      modifier = buttonModifier,
+      onClick = onPreviousTrackClick,
+      colors = buttonColors,
+      shape = CircleShape
     ) {
-        PlayerButton("\u23EE", onPreviousTrackClick)
-        PlayerButton(if (isAudioPlaying) "\u23F8" else "\u25B6", onPlayButtonClick)
-        PlayerButton("\u23ED", onNextTrackClick)
+      Icon(
+        modifier = Modifier.size(24.dp),
+        painter = painterResource("backward_arrows.svg"),
+        contentDescription = "Previous track",
+        tint = buttonTint
+      )
     }
+    Button(
+      modifier = buttonModifier,
+      onClick = onPlayButtonClick,
+      colors = buttonColors,
+      shape = CircleShape
+    ) {
+      Icon(
+        modifier = Modifier.size(24.dp),
+        painter = painterResource(
+          if (isAudioPlaying) "double_rectangle.svg" else "forward_arrow.svg"
+        ),
+        contentDescription = "Play track",
+        tint = buttonTint
+      )
+    }
+    Button(
+      modifier = buttonModifier,
+      onClick = onNextTrackClick,
+      colors = buttonColors,
+      shape = CircleShape
+    ) {
+      Icon(
+        modifier = Modifier.size(24.dp),
+        painter = painterResource("forward_arrows.svg"),
+        contentDescription = "Next track",
+        tint = buttonTint
+      )
+    }
+  }
 }
--- src/leodenault/synced/preview/Preview.kt
+++ src/leodenault/synced/preview/Preview.kt
@@ -2,16 +2,21 @@ package leodenault.synced.preview
 
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
 import kotlinx.coroutines.runBlocking
 import leodenault.synced.coroutines.CoroutineScopes
+import leodenault.synced.player.PlayerControls
 
 @Composable
 fun Preview() {
+  val isAudioPlaying = remember { mutableStateOf(false) }
 
+  PlayerControls(isAudioPlaying = isAudioPlaying.value, onPlayButtonClick = {isAudioPlaying.value = !isAudioPlaying.value})
 }
 
 fun main() {
